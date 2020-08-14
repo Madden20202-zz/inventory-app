78efa62587cabbced681cea219639047dc52ba5a
Austin <business.mocksock@gmail.com>
2020-08-14 10:53:43 -0700

    Added inventory logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventory-app';
+
+  constructor() {
+    //Inventory Logic
+    let newProduct = new Product(
+      "NICEHAT", //SKU
+      "A Nice Black Hat", //Name
+      "/assets/images/products/a_nice_black_hat.png", //image URL
+      ["Men's", "Accessories", "Hats"], //Department
+      29.99 //Price
+    );
+  }
 }
