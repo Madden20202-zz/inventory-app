9adb1b81bc0f702e2edf42434125fc9fb9460b89
Austin <business.mocksock@gmail.com>
2020-08-28 08:25:01 -0700

    Cleaned up notes and removed redundant code

--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -6,10 +6,8 @@ export class Product {
         public sku: string,
         public name: string,
         public imageUrl: string,
-        public department: string[], //why add the brackets?
-        public price: number
-    ) {
-        //Code to use these variables will come in later!
+        public department: string[],
+        public price: number) {
     }
 
 }
